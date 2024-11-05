fe33565a9d915b956a9ee7ba3e33644297486a9e
Nick <nickgomvelez@gmail.com>
2024-11-05 10:49:24 -0500

    feat: create and style the SearchBar component

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,31 @@
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+
+export const SearchBar = () => {
+  const { searchTerm, setSearchTerm } = useState("");
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  return (
+    <div className="w-full md:px-4 md:w-50 lg:w-80">
+      <div className="relative">
+        <input
+          type="text"
+          placeholder="Search books..."
+          onChange={handleSearchChange}
+          value={searchTerm}
+          className="w-full bg-transparent rounded-md border border-stroke dark:border-dark-3 py-2.5 px-4 text-dark-6 outline-none transition focus:border-white focus:ring focus:ring-orange-400"
+        />
+        <button
+          type="button"
+          className="absolute inset-y-0 right-0 px-3 py-2.5 text-gray-500 hover:text-gray-700 focus:outline-none"
+        >
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </button>
+      </div>
+    </div>
+  );
+};
