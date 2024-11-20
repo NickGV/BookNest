3b481fdf3bed13908b0ab3871997117b50a69e97
Nick <nickgomvelez@gmail.com>
2024-11-20 11:02:28 -0500

    feat:create books services

--- frontend/src/api/bookService.js
+++ frontend/src/api/bookService.js
@@ -9,6 +9,60 @@ export const fetchBooks = async (query) => {
     }
   );
   const data = await response.json();
-  console.log(data.items)
+  console.log(data.items);
   return data.items;
 };
+
+const API_URL = "http://localhost:3000/api";
+
+export const fetchUserBooks = async (token) => {
+  const response = await fetch(`${API_URL}/books`, {
+    headers: {
+      Authorization: token,
+    },
+  });
+  return response.json();
+};
+
+export const saveBook = async (token, book) => {
+  const response = await fetch(`${API_URL}/books`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(book),
+  });
+  return response.json();
+};
+
+export const updateBook = async (token, bookId, status) => {
+  const response = await fetch(`${API_URL}/books/${bookId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ status }),
+  });
+  return response.json();
+};
+
+export const deleteBook = async (token, bookId) => {
+  const response = await fetch(`${API_URL}/books/${bookId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.json();
+};
+
+export const getUserBooks = async (token) => {
+  const response = await fetch(`${API_URL}/books`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.json();
+}
