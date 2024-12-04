9075917dbed1ede1f08d7b0e3a8b0460f71347d0
Nick <nickgomvelez@gmail.com>
2024-12-04 14:50:55 -0500

    chance vercel url

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://bookstore-frontend-5173.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://book-nest-three.vercel.app/",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
